ccaacf02b6ac1f3cc08e13f6eb4b85e26bd24ac7
Nick Bernd Espig <info@nachtalb.io>
2020-02-05 19:19:22 +0100

    Improve builtin home

--- bot/commands/builtins.py
+++ bot/commands/builtins.py
@@ -1,8 +1,9 @@
 from django.template.loader import get_template
-from telegram import ReplyKeyboardMarkup
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import CallbackQueryHandler, MessageHandler
 
 from bot.commands import BaseCommand
+from bot.models.promo_group import TelegramUser
 from bot.filters import Filters as OwnFilters
 from bot.utils.chat import build_menu
 
@@ -13,24 +14,30 @@ class Builtins(BaseCommand):
     def help(self):
         self.message.reply_html(get_template('commands/builtins/help.html').render())
 
-    @BaseCommand.command_wrapper(CallbackQueryHandler, pattern='^(home|cancel)$')
-    @BaseCommand.command_wrapper(MessageHandler, filters=OwnFilters.text_is(['cancel', 'home', 'reset'], lower=True))
-    @BaseCommand.command_wrapper(names=['start', 'reset', 'cancel'])
+    @BaseCommand.command_wrapper(CallbackQueryHandler, pattern='^(home|reset)$')
+    @BaseCommand.command_wrapper(MessageHandler, filters=OwnFilters.text_is(['home', 'reset'], lower=True))
+    @BaseCommand.command_wrapper(names=['start', 'reset'])
     def start(self):
         if self.update.callback_query:
             self.update.callback_query.answer()
             self.message.delete()
 
-        elif 'start' in self.message.text:
-            self.message.reply_html(get_template('commands/builtins/start.html').render())
-
-        if self.message.text.lower() in ['cancel', 'home', 'reset']:
-            self.message.reply_text('Current action was cancelled')
-
         self.telegram_user.current_channel = None
-        header, middle, footer = BaseCommand._start_buttons
+        self.telegram_user.set_menu(TelegramUser.MAIN)
 
-        buttons = build_menu(*middle, header_buttons=header, footer_buttons=footer)
-        self.message.reply_text('What do you want to do?', reply_markup=ReplyKeyboardMarkup(buttons))
+        header, middle, footer = BaseCommand._start_buttons
+        menu = build_menu(*middle, header_buttons=header, footer_buttons=footer)
+        if not menu:
+            buttons = ReplyKeyboardRemove()
+        else:
+            buttons = ReplyKeyboardMarkup(menu)
+
+        message = self.message.text.strip('/').lower()
+        if message in ['home', 'reset']:
+            text = 'Current action was cancelled'
+        elif message in ['start']:
+            text = get_template('commands/builtins/start.html').render()
+
+        self.message.reply_html(text, reply_markup=buttons)
 
     BaseCommand.register_home(start)
